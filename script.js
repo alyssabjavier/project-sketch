ccabc64741032506f53acaec2737e47b09a5b869
“alyssabjavier” <alyssabjavier@gmail.com>
2024-02-07 14:17:56 -0600

    Successfully set up hover effect

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const container = document.getElementById('container');
 const numSquare = 256;
 for (let i = 0; i < numSquare; i++) {
     const squareDiv = document.createElement('div');
-   // squareDiv.textContent = 's';
     squareDiv.className = 'square';
     container.appendChild(squareDiv);
+    
+//change background when mouse hovers
+    squareDiv.addEventListener('mouseover', (event) => {
+        squareDiv.style.backgroundColor = 'black';
+    }
+    );
+
 }

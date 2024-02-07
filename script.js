f16e6dbc9a8abe236abde44b28a15d78ccf66d16
“alyssabjavier” <alyssabjavier@gmail.com>
2024-02-07 16:03:06 -0600

    Get number of squares by prompting user using button

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const container = document.getElementById('container');
-const numSquare = 256;
-for (let i = 0; i < numSquare; i++) {
-    const squareDiv = document.createElement('div');
-    squareDiv.className = 'square';
-    container.appendChild(squareDiv);
-    
-//change background when mouse hovers
-    squareDiv.addEventListener('mouseover', (event) => {
-        squareDiv.style.backgroundColor = 'black';
-    }
-    );
+const promptBtn = document.getElementById('promptBtn');
+promptBtn.addEventListener('click', (event) => {
+    const num = prompt('how large do u want ur grid, pls keep it under 1 hundo');
+    const numSquare = num * num;
 
-}
+    for (let i = 0; i < numSquare; i++) {
+        const squareDiv = document.createElement('div');
+        calcSize = 500 / num;
+        squareDiv.style.width = calcSize + 'px';
+        squareDiv.style.height = calcSize + 'px';
+        squareDiv.className = 'square';
+        container.appendChild(squareDiv);
+        
+    //change background when mouse hovers
+        squareDiv.addEventListener('mouseover', (event) => {
+            squareDiv.style.backgroundColor = 'black';
+        }
+        );
+    
+    }
+})
